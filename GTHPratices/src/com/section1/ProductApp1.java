9d35ad95205b05ad1f4d5407353c382133a6000c
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2024-12-11 08:56:42 +0530

    Woking

--- GTHPratices/src/com/section1/ProductApp1.java
+++ GTHPratices/src/com/section1/ProductApp1.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.Scanner;
 
 public class ProductApp1 {
+	static List<Product> list=new ArrayList<>();
 	static Scanner sc=new Scanner(System.in);
 	public static boolean addStock()
 	{
@@ -24,7 +25,18 @@ public class ProductApp1 {
 			System.out.println("Enter the Category");
 			sc.nextLine();
 			String categary=sc.nextLine();
-			return true;
+			if(list.add(new Product(id,pname,price,quantity,categary)))
+			{
+				System.out.println("Product added");
+				return true;  
+			}
+			else
+			{
+				System.out.println("Product not added");
+				return false;
+			}
+			
+			
 			
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -48,7 +60,7 @@ public class ProductApp1 {
 	
 	}
 	
-	public void findHigestStock(List<Product> p)
+	public static void findHigestStock(List<Product> p)
 	{
 		Optional<Product> max=p.stream().max(Comparator.comparingInt(pr -> pr.getPrice()));
 		max.ifPresent(pro-> System.out.println(pro));
@@ -60,13 +72,68 @@ public class ProductApp1 {
 	
 	public static void main(String[] args) {
 		
-		List<Product> list=new ArrayList<>();
+		
 		list.add(new Product(1, "Harry Potter", 200, 1,  "Book"));
 		list.add(new Product(2, "Harry Potter", 200, 1,  "Book"));
 		list.add(new Product(3, "Harry Potter", 200, 1,  "Book"));
 		list.add(new Product(4, "Harry Potter", 200, 1,  "Book"));
 		list.add(new Product(5, "Harry Potter", 200, 1,  "Book"));
 		
+		System.out.println("Enter the Choice ");
+		System.out.println("1: Update or delete product");
+		System.out.println("2: Display with Category ");
+		System.out.println("3:find Product with higest price");
+		int choice=sc.nextInt();
+		switch(choice)
+		{
+		case 1:
+			System.out.println("1: Enter to add");
+			System.out.println("2: Enter to remove");
+			int c=sc.nextInt();
+			switch(c)
+			{
+			case 1:
+				addStock();
+				break;
+			case 2:
+				try
+				{
+					System.out.println("Enter the Id to Delete");
+					int id=sc.nextInt();
+					if(removeStock(list,id))
+					{
+						System.out.println("Stoct Deletedd");
+					}
+					else
+					{
+						System.out.println("Stock not found");
+					}
+					
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+				default:
+					System.out.println("Invalid Input");
+			}
+			break;
+		case 2:
+			try
+			{
+				System.out.println("Enter the Category ");
+				String ca=sc.next();
+				display(list,ca);
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
+		case 3:
+			findHigestStock(list);
+			break;
+			default :
+				System.out.println("Invalid Choice");
+		}
 		
 		
 		
